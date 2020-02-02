6907958199836338186721cf20e82475ec70abde
johtela <tommi.johtela@dnainternet.net>
2020-02-02 22:25:35 +0200

    Testing stuff in progress.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
 export * from './dependency-diag'
-export { test } from './tester'
 export * from './test-visualizer'
--- src/tester.ts
+++ src/tester.ts
@@ -2,7 +2,21 @@ import * as zora from 'zora'
 
 let harness = zora.createHarness()
 let testfn: zora.TestFunction = harness.test
-harness.report(zora.mochaTapLike)
+runTests()
+
+async function runTests() {
+    try {
+        await harness.report(zora.mochaTapLike)
+    }
+    catch {
+        harness.pass = false
+    }
+    if (harness.pass)
+        console.log('Tests PASSED')
+    else
+        console.log('Tests FAILED')
+    process.exit(harness.pass ? 0 : 1)
+}
 
 export function test(description: string, spec: zora.SpecFunction, 
     options?: object): Promise<zora.TestResult> {
