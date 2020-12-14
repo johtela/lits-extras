f7ea681ba27a8f3b2697fd8334a8116dab6830c5
johtela <tommi.johtela@dnainternet.net>
2020-12-14 15:37:30 +0200

    Rewrote test visualizer without lit-html

--- src/test-visualizer.ts
+++ src/test-visualizer.ts
@@ -1,12 +1,23 @@
 import * as tr from './test-reporter'
 import * as tester from './tester'
-import { html, render } from 'lit-html'
-import { styleMap } from 'lit-html/directives/style-map'
 
 export function runTests(params: string, parent: HTMLElement) {
-    window.addEventListener('load', () =>
-        tester.getHarness().report(tr.createReporter(params, status => 
-            render(renderTestStatus(status), parent))))
+    window.addEventListener('load', () => {
+        while (parent.firstChild)
+            parent.firstChild.remove()
+        tester.getHarness().report(tr.createReporter(params, status =>
+            parent.appendChild(testStatus(status))))
+    })
+}
+
+function elem<K extends keyof HTMLElementTagNameMap>(tagName: K, cls?: string,
+    text?: string): HTMLElement {
+    let res = document.createElement(tagName)
+    if (cls)
+        res.classList.add(cls)
+    if (text)
+        res.innerText = text
+    return res
 }
 
 function statusIcon(assertion: tr.Assertion): string {
@@ -14,52 +25,59 @@ function statusIcon(assertion: tr.Assertion): string {
 }
 
 function testStyle(test: tr.Test) {
-    return {
-        backgroundColor: test.pass ? '#f8fff8' : '#fff8f8'
-    }
+    return test.pass ? '#f8fff8' : '#fff8f8'
 }
 
-const renderTestStatus = (rootTest: tr.Test) => html`
-    <div class="test-visualizer" style=${styleMap(testStyle(rootTest))}>
-        <div class="summary">
-            ${statusIcon(rootTest)} ${rootTest.name}
-            <span class="count">Pass: ${rootTest.passes}</span>
-            <span class="count">Fail: ${rootTest.fails}</span>
-        </div>
-        ${renderTestList(rootTest.tests)}
-    </div>`
+function testStatus(rootTest: tr.Test): HTMLElement {
+    let vis = elem("div","test-visualizer")
+    vis.style.backgroundColor = testStyle(rootTest)
+    let sum = elem("div", "summary", `${statusIcon(rootTest)} ${rootTest.name}`)
+    sum.appendChild(elem("span", "count", `Pass: ${rootTest.passes}`))
+    sum.appendChild(elem("span", "count", `Fail: ${rootTest.fails}`))
+    vis.appendChild(sum)
+    vis.appendChild(testList(rootTest.tests))
+    return vis
+}
 
-const renderTestList = (tests: tr.Test[]) => html`
-    <ol class="test-list">
-        ${tests.map(renderTest)}
-    </ol>`
+function testList(tests: tr.Test[]): HTMLElement {
+    let lst = elem("ol", "test-list")
+    for (let i = 0; i < tests.length; i++)
+        lst.appendChild(test(tests[i]))
+    return lst
+}
 
-const renderAssertion = (assertion: tr.Assertion) => html`
-    <li>
-        ${statusIcon(assertion)} ${assertion.name}     
-    </li>`
+function assertion(assertion: tr.Assertion): HTMLElement {
+    return elem("li", undefined, `${statusIcon(assertion)} ${assertion.name}`)
+}
 
-const renderAssertions = (assertions: tr.Assertion[]) => html`
-    <details>
-        <summary>${assertions.length} assertions</summary>
-        <ol>
-            ${assertions.map(renderAssertion)}
-        </ol>
-    </details>`
+function assertions(assertions: tr.Assertion[]): HTMLElement {
+    let det = elem("details")
+    det.appendChild(
+        elem("summary", undefined, `${assertions.length} assertions`))
+    let ol = elem("ol")
+    for (let i = 0; i < assertions.length; i++) 
+        ol.appendChild(assertion(assertions[i]))
+    det.appendChild(ol)
+    return det
+}
 
-const renderTest = (test: tr.Test) => 
-    test.error ? 
-        renderBailedOutTest(test) :
-        html` 
-            <li>
-                ${statusIcon(test)} ${test.name} in ${test.duration}ms
-                ${test.assertions ? renderAssertions(test.assertions) : ""}
-                ${test.tests ? renderTestList(test.tests) : ""}
-            </li>`
+function test(test: tr.Test): HTMLElement {
+    if (test.error)
+        return bailedOutTest(test)
+    let li = elem("li", undefined, 
+        `${statusIcon(test)} ${test.name} in ${test.duration}ms`)
+    if (test.assertions)
+        li.appendChild(assertions(test.assertions))
+    if (test.tests)
+        li.appendChild(testList(test.tests))
+    return li
+}
 
-const renderBailedOutTest = (test: tr.Test) => html`
-    <li>
-        ${statusIcon(test)} ${test.name} threw <b>${test.error.name}</b> exception:
+function bailedOutTest(test: tr.Test): HTMLElement {
+    let res = document.createElement("li")
+    res.innerHTML =
+        `${statusIcon(test)} ${test.name} threw <b>${test.error.name}</b> exception:
         <br/><b>${test.error.message}</b>
-        <pre>${test.error.stack}</pre>
-    </li>`
+        <pre>${test.error.stack}</pre>`
+    return res
+}
